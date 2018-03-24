a948283803557fdfe17d1c63c8ea0c09a81a294f
ilonabasovska <ilonabasovska@gmail.com>
2018-03-24 17:21:07 +0100

    first commit

---
+++ UniversalPointOperation/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace UniversalPointOperation
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.histpgramPictureBox = new System.Windows.Forms.PictureBox();
+            this.linesPictureBox = new System.Windows.Forms.PictureBox();
+            this.bezierPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.openImagePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histpgramPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linesPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bezierPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openImagePictureBox
+            // 
+            this.openImagePictureBox.Location = new System.Drawing.Point(13, 13);
+            this.openImagePictureBox.Name = "openImagePictureBox";
+            this.openImagePictureBox.Size = new System.Drawing.Size(360, 320);
+            this.openImagePictureBox.TabIndex = 0;
+            this.openImagePictureBox.TabStop = false;
+            // 
+            // histpgramPictureBox
+            // 
+            this.histpgramPictureBox.Location = new System.Drawing.Point(379, 13);
+            this.histpgramPictureBox.Name = "histpgramPictureBox";
+            this.histpgramPictureBox.Size = new System.Drawing.Size(360, 320);
+            this.histpgramPictureBox.TabIndex = 1;
+            this.histpgramPictureBox.TabStop = false;
+            this.histpgramPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.histpgramPictureBox_Paint);
+            this.histpgramPictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.histpgramPictureBox_MouseClick);
+            // 
+            // linesPictureBox
+            // 
+            this.linesPictureBox.Location = new System.Drawing.Point(13, 339);
+            this.linesPictureBox.Name = "linesPictureBox";
+            this.linesPictureBox.Size = new System.Drawing.Size(360, 320);
+            this.linesPictureBox.TabIndex = 2;
+            this.linesPictureBox.TabStop = false;
+            // 
+            // bezierPictureBox
+            // 
+            this.bezierPictureBox.Location = new System.Drawing.Point(379, 339);
+            this.bezierPictureBox.Name = "bezierPictureBox";
+            this.bezierPictureBox.Size = new System.Drawing.Size(360, 320);
+            this.bezierPictureBox.TabIndex = 3;
+            this.bezierPictureBox.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 686);
+            this.Controls.Add(this.bezierPictureBox);
+            this.Controls.Add(this.linesPictureBox);
+            this.Controls.Add(this.histpgramPictureBox);
+            this.Controls.Add(this.openImagePictureBox);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.openImagePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histpgramPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linesPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bezierPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox openImagePictureBox;
+        private System.Windows.Forms.PictureBox histpgramPictureBox;
+        private System.Windows.Forms.PictureBox linesPictureBox;
+        private System.Windows.Forms.PictureBox bezierPictureBox;
+    }
+}
+
---
+++ UniversalPointOperation/MainForm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversalPointOperation
+{
+    public partial class MainForm : Form
+    {
+        int x1;
+        int y1;
+        private PointF[] histo = new PointF[256];
+        private List<PointF> point = new List<PointF>();
+        private List<PointF> pointBezier = new List<PointF>();
+        private PointF[] pointsForBezierCurves = new PointF[7];
+        private Point[] histoBezier = new Point[256];
+        public MainForm()
+        {
+            InitializeComponent();
+            MakeHisto();
+            point.Add(new PointF() { X = 0, Y = 0 });
+            point.Add(new PointF() { X = 255, Y = 255 });
+            openImagePictureBox.Image = new Bitmap("C:\\Users\\family\\Desktop\\lena-256x256.bmp");
+        }
+
+        private void MakeHisto()
+        {
+            for (int i = 0; i < 256; ++i)
+            {
+                histo[i] = new PointF(i, i);
+            }
+        }
+        private void ChangeImage(int x, float y)
+        {
+            point.Add(new PointF() { X = x1, Y = y1 });
+            point = point.OrderBy(p => p.X).ToList();
+
+            for (int c = 0; c < point.Count - 1; ++c)
+            {
+                for (int i = (int)point[c].X; i <= point[c + 1].X; ++i)
+                {
+                    histo[i] = new PointF(i, ((((i - point[c + 1].X) / (point[c].X - point[c + 1].X)) * (point[c].Y - point[c + 1].Y)) + point[c + 1].Y));
+                }
+            }
+        }
+
+        private void MakeNewImage()
+        {
+            Bitmap bitmap = new Bitmap(openImagePictureBox.Image);
+            Bitmap bitmapNewImage = new Bitmap(bitmap.Width, bitmap.Height);
+            int[,] histogram = new int[bitmap.Width, bitmap.Height];
+            int[,] histoNewImage = new int[bitmap.Width, bitmap.Height];
+            Color pixelColor = new Color();
+            for (int x = 0; x < bitmap.Width; ++x)
+            {
+                for (int y = 0; y < bitmap.Height; ++y)
+                {
+                    pixelColor = bitmap.GetPixel(x, y);
+                    histogram[x, y] = ((pixelColor.R * 229 + pixelColor.G * 589 + pixelColor.B * 114) / 1000);
+                }
+            }
+            for (int x = 0; x < bitmap.Width; ++x)
+            {
+                for (int y = 0; y < bitmap.Height; ++y)
+                {
+                    for (int c = 0; c < histo.Length; ++c)
+                    {
+                        if (histogram[x, y] == histo[c].X)
+                        {
+                            histoNewImage[x, y] = (int)histo[c].Y;
+                        }
+                    }
+                }
+            }
+            for (int x = 0; x < bitmap.Width; ++x)
+            {
+                for (int y = 0; y < bitmap.Height; ++y)
+                {
+                    bitmapNewImage.SetPixel(x, y, Color.FromArgb(histoNewImage[x, y], histoNewImage[x, y], histoNewImage[x, y]));
+                }
+            }
+            linesPictureBox.Image = bitmapNewImage;
+        }
+
+
+        private void MakePointsForBezierCurves()
+        {
+            float sum = 0;
+            for (int c = 0; c < point.Count - 1; ++c)
+            {
+                pointBezier.Add(new PointF(point[c].X, point[c].Y));
+                pointBezier.Add(new PointF(((point[c + 1].X - point[c].X) / 4) + sum, point[c].Y + 50));
+                pointBezier.Add(new PointF((3 * (point[c + 1].X - point[c].X) / 4) + sum, point[c + 1].Y - 50));
+                sum += point[c + 1].X - point[c].X;
+            }
+            pointBezier.Add(new PointF(point[point.Count - 1].X, point[point.Count - 1].Y));
+            pointBezier = pointBezier.OrderBy(p => p.X).ToList();
+            pointsForBezierCurves = pointBezier.ToArray();
+        }
+
+        private void ChangeImageBezier()
+        {
+
+
+            for (int i = 0; i < pointBezier[3].X; i++)
+            {
+                //for (int c = 0; c < pointBezier.Count - 1; c = +3)
+                //{
+                float t = (float)i / (pointBezier[3].X);
+
+                float x = i;
+                //float x = (1 - t) * (1 - t) * (1 - t) * pointBezier[0].X +
+                //           3 * t * (1 - t) * (1 - t) * pointBezier[1].X +
+                //           3 * t * t * (1 - t) * pointBezier[2].X +
+                //           t * t * t * pointBezier[3].X;
+
+                float y = (1 - t) * (1 - t) * (1 - t) * pointBezier[0].Y +
+                           3 * t * (1 - t) * (1 - t) * pointBezier[1].Y +
+                           3 * t * t * (1 - t) * pointBezier[2].Y +
+                           t * t * t * pointBezier[3].Y;
+
+                histoBezier[i] = new Point((int)x, (int)(y));
+                //}
+            }
+            int a = 0;
+            for (int i = (int)pointBezier[3].X; i <= pointBezier[6].X; i++)
+            {
+                float t = (float)a / (pointBezier[6].X - pointBezier[3].X);
+
+                float x = i;
+                //float x = (1 - t) * (1 - t) * (1 - t) * pointBezier[3].X +
+                //           3 * t * (1 - t) * (1 - t) * pointBezier[4].X +
+                //           3 * t * t * (1 - t) * pointBezier[5].X +
+                //           t * t * t * pointBezier[6].X;
+
+                float y = (1 - t) * (1 - t) * (1 - t) * pointBezier[3].Y +
+                           3 * t * (1 - t) * (1 - t) * pointBezier[4].Y +
+                           3 * t * t * (1 - t) * pointBezier[5].Y +
+                           t * t * t * pointBezier[6].Y;
+
+                histoBezier[i] = new Point((int)x, (int)(y));
+                a++;
+            }
+        }
+        private void MakeNewImageBezier()
+        {
+            Bitmap bitmap = new Bitmap(openImagePictureBox.Image);
+            Bitmap bitmapNewImage = new Bitmap(bitmap.Width, bitmap.Height);
+            int[,] histogram = new int[bitmap.Width, bitmap.Height];
+            int[,] histoNewImage = new int[bitmap.Width, bitmap.Height];
+            Color pixelColor = new Color();
+            for (int x = 0; x < bitmap.Width; ++x)
+            {
+                for (int y = 0; y < bitmap.Height; ++y)
+                {
+                    pixelColor = bitmap.GetPixel(x, y);
+                    histogram[x, y] = ((pixelColor.R * 229 + pixelColor.G * 589 + pixelColor.B * 114) / 1000);
+                }
+            }
+            for (int x = 0; x < bitmap.Width; ++x)
+            {
+                for (int y = 0; y < bitmap.Height; ++y)
+                {
+                    for (int c = 0; c < histoBezier.Length; ++c)
+                    {
+                        if (histogram[x, y] == histoBezier[c].X)
+                        {
+                            histoNewImage[x, y] = (int)histoBezier[c].Y;
+                        }
+                    }
+                }
+            }
+            for (int x = 0; x < bitmap.Width; ++x)
+            {
+                for (int y = 0; y < bitmap.Height; ++y)
+                {
+                    bitmapNewImage.SetPixel(x, y, Color.FromArgb(histoNewImage[x, y], histoNewImage[x, y], histoNewImage[x, y]));
+                }
+            }
+            bezierPictureBox.Image = bitmapNewImage;
+        }
+        private void histpgramPictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            x1 = e.X;
+            y1 = e.Y;
+            ChangeImage(x1, y1);
+            MakeNewImage();
+            MakePointsForBezierCurves();
+            ChangeImageBezier();
+            MakeNewImageBezier();
+            histpgramPictureBox.Invalidate();
+        }
+
+        private void histpgramPictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle rectangle = new Rectangle(0, 0, 255, 255);
+            e.Graphics.DrawRectangle(new Pen(Color.Black, 2), rectangle);
+
+            e.Graphics.DrawLines(new Pen(Color.Magenta, 2), histo);
+
+            e.Graphics.DrawBeziers(new Pen(Color.Blue, 2), pointsForBezierCurves);
+
+        }
+    }
+}
